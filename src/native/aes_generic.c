47a98f14cb65080842bc5b872b1f518b23b0954c
SaiVK <saiganesha5.svkv@gmail.com>
2022-06-15 12:00:08 +0530

    Added debug statements

--- src/native/aes_generic.c
+++ src/native/aes_generic.c
@@ -3171,11 +3171,28 @@ mc_aes_derive_d_key_generic (value key, value off1, value kr, value rounds, valu
 
 CAMLprim value
 mc_aes_enc_generic (value src, value off1, value dst, value off2, value rk, value rounds, value blocks) {
+  int num_elts_rk = 1;
+  for (int i = 0; i < Caml_ba_array_val(rk)->num_dims; i++) num_elts_rk = num_elts_rk * Caml_ba_array_val(rk)->dim[i];
+  printf("src : %x, off1 : %x, dst : %x, off2 : %x, rk : %x, rounds : %x, blocks : %x, num_elts_rk = %x\n", src, off1, dst, off2, rk, rounds, blocks, num_elts_rk);
+
+
+  printf("Cipher text 1: ");
+  for(int i=0; i<16; i++){
+    printf("%x", ((uint8_t*)Caml_ba_data_val(dst))[i]);
+  }
+  printf("\n");
+
   _mc_aes_enc_blocks ( _ba_uint8_off (src, off1),
                        _ba_uint8_off (dst, off2),
                        _ba_uint32 (rk),
                        Int_val (rounds),
                        Int_val (blocks) );
+
+  printf("Cipher text 2: ");
+  for(int i=0; i<16; i++){
+    printf("%x", ((uint8_t*)Caml_ba_data_val(dst))[i]);
+  }
+  printf("\n");
   return Val_unit;
 }
 
